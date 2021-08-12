ae970b7f599bf9350c466eee5d1803899723c206
Nathaniel Richards <nathaniel@datacompusa.com>
2021-08-12 11:30:38 -0400

    Fixed statistics for room counts

--- src/resolvers/query/properties/getStatistics.ts
+++ src/resolvers/query/properties/getStatistics.ts
@@ -11,9 +11,13 @@ export const getStatistics: Resolvers.QueryResolvers['getStatistics'] = async (
 
 	const totalProperties = await (await Property.query()).length;
 
-	const totalOpenRooms = await (await Room.query()).length;
+	const totalOpenRooms = await (
+		await Room.query().where({ available: true })
+	).length;
 
-	const totalUnavailableRooms = await (await Room.query()).length;
+	const totalUnavailableRooms = await (
+		await Room.query().where({ available: false })
+	).length;
 
 	const totalRooms = await (await Room.query()).length;
 
